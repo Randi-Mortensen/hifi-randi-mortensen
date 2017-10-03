122a34f9c40c7815373b360b54a5ab96fa9e84d0
Randi Mortensen <3884187@rts-365.dk>
2017-10-03 20:36:48 +0200

    update

--- public/public/assets/js/produkt.js
+++ public/public/assets/js/produkt.js
@@ -57,5 +57,70 @@ function udskrivProdukter() {
 }
 
 
-    // ========================================================
+// ========================================================
+fetch('http://localhost:1337/produkt')
+    .then((response) => {
+        // grib svarets indhold (body) og send det som et json objekt til næste .then()
+        return response.json();
+    })
+    .then((data) => {
+        // nu er json objektet lagt ind i data variablen, udskriv data
+        console.log("her burde komme produkter");
+        console.log(data);
+        var content = document.getElementById('content');
+        console.log("content");
+        console.log("udskriv content");
+
+        var type = '';
+        document.getElementById('content').innerHTML = "";
+        data.forEach(function (item) {
+            document.getElementById('content').innerHTML += `<h2>${item.type_navn}</h2>`;
+            item.prod.forEach(function (prod) {
+                document.getElementById('content').innerHTML += `
+                        <div>
+                        <img src="assets/media/${prod.billede}" width="100px" /><br>
+                            <b>Produkt Navn: ${prod.navn}</b><br>
+                            <b>Producent: ${prod.producent}</b><br>
+                            <b>pris: kr. ${prod.pris}<b><br>
+                            Beskrivelse: ${prod.beskrivelse} 
+                        </div>  
+                        `;
+            }, this);
+
+        })
+    })
+        /*
+
+        // TEORI!! -- Slet alle variabler der hedder noget med "forste".
+
+        //data[0] // både "type" og "prod" (array)
+
+        var forste_kategori = data[0];
+        var forste_produkt = data[0].prod[0];
+
+        // console.log("Første kategori:");
+        // console.log(forste_kategori);
+
+        console.log("Første produkts produkt_navn:");
+        console.log(forste_produkt.navn)
+        */
+
+        // PSEUDO-KODE
+        /*
+
+        data.foreach (kategori) {
+            console.log (kategori.type)
+            data.foreach (produkt) {
+                console.log (produkt.navn)
+            }
+        }
+
+
+
+        */
+
+
+
+
+        // content.innerHTML = data[0].produkt_navn + " " + data[0].producent_navn + " " + data[0].type_navn + " " + data[0].produkt_billede + " " + data[0].produkt_varenr + " " + data[0].produkt_pris;
 
