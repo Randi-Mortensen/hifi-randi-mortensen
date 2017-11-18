164b20a2d197a6751f339c708869ed6cecd0890f
Randi Mortensen <3884187@rts-365.dk>
2017-11-18 15:56:57 +0100

    change database

--- api/app.js
+++ api/app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const port = process.env.port || 3000;
 
 const app = restify.createServer({
-   name: 'Administrations Panel Eksempel',
-   version: '1.0.0'
+    name: 'hifi2',
+    version: '1.0.0'
 });
 
 const logger = require('morgan');
@@ -14,17 +14,17 @@ app.use(logger('dev'));
 app.use(restify.plugins.acceptParser(app.acceptable));
 // bodyparser skal vide hvor billederne skal placeres 
 app.use(restify.plugins.bodyParser({
-   mapParms: true,
-   mapFiles: true,
-   keepExtensions: true,
-   uploadDir: './api/tmp_upload'
+    mapParms: true,
+    mapFiles: true,
+    keepExtensions: true,
+    uploadDir: './api/tmp_upload'
 }));
 app.use(restify.plugins.queryParser());
 
 require(path.join(__dirname, 'routes', 'products'))(app);
 
 app.listen(port, function (err) {
-   if (err) console.log(err);
-   console.log('========================================================================================');
-   console.log('%s is listening on port %s, address: %s', app.name, port, 'http://localhost:' + port);
+    if (err) console.log(err);
+    console.log('========================================================================================');
+    console.log('%s is listening on port %s, address: %s', app.name, port, 'http://localhost:' + port);
 });
